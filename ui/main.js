5188bea8c8c6c069d9d2c62efee144a9ed03abee
radforty <30650225+radforty@users.noreply.github.com>
2017-08-22 23:58:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 //Change the text of the maon text div
 var element=document.getElementById('main-text');
 element.innerHTML="New Value";
+
+//move the image
+var img = document.getElementById('img');
+img.onclick=function()
+{
+    img.style.marginleft='100px';
+};
