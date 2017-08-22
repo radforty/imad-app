a31c2ef3b3cc0c9b4573ab12cea01ab4dfe5ab22
radforty <30650225+radforty@users.noreply.github.com>
2017-08-23 00:05:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,8 @@ var element=document.getElementById('main-text');
 element.innerHTML="New Value";
 
 //move the image
-var img = document.getElementById('ui/madi');
-img.onclick=function()
+var img = document.getElementById('madi');
+img.onclick = function()
 {
     img.style.marginLeft='100px';
 };
