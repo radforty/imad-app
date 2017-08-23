06f1e844bda65d9b0076fe83521fbe3d06d6d9b9
radforty <30650225+radforty@users.noreply.github.com>
2017-08-23 13:00:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,13 @@
-console.log('Loaded!');
-
-//Change the text of the main text div
-var element=document.getElementById('main-text');
-element.innerHTML="New Value";
-
-//move the image
-var img = document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft + 5;
-     img.style.marginLeft=marginLeft+ 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
+//counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onClick = function(){
+    //make a request to the counter end point
+    
+    //capture the response and store it in a variable
+    
+    //render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHtml = counter.toString();
 };
