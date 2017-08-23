d8d404ec84ac9f1f17fc3022c7b6c3c5aa44507f
radforty <30650225+radforty@users.noreply.github.com>
2017-08-23 13:39:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,6 +18,6 @@ button.onclick = function(){
     //not done yet
     };
     //make the request
-    request.open('GET', 'helloradhika.imad.hasura-app.io/counter', true);
+    request.open('GET', 'http://helloradhika.imad.hasura-app.io/counter', true);
     request.send(null);
 };
