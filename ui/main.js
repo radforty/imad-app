f81fe0bb72c4c845c1d0169f212991ca26021cb1
radforty <30650225+radforty@users.noreply.github.com>
2017-08-24 00:08:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -52,7 +52,7 @@ submit.onclick=function(){
            }
          };
          var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
+         var submit = document.getElementById('submit_btn');
           request.open('GET', 'http://helloradhika.imad.hasura-app.io/submit-name?name='+name, true);
-    request.send(null);
+         request.send(null);
         };
