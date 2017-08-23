88863af280eac0a1b5769d6c52da6613422e17d0
radforty <30650225+radforty@users.noreply.github.com>
2017-08-23 22:28:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,23 @@ button.onclick = function(){
     request.open('GET', 'http://helloradhika.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+//submit name
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onClick=function(){
+    
+    //make a request to the server and send the name
+    
+    //capture the list of names and render it as a list
+    
+    var names = ['name', 'name2', 'name3', 'name4'];
+    var list='';
+    for(var i=0; i<names.length; i++) {
+        list += '<li>' + names[i] + '<li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML=list;
+};
