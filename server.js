3f1ca319e4b5f7c1e973870471cec0dbafe900c2
radforty <30650225+radforty@users.noreply.github.com>
2017-08-23 23:24:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,9 +109,9 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name/:name', function(req,res){
+app.get('/submit-name', function(req,res){ // URL Submit-name/name:xxxx
     //get the names from the request
-    var name=req.params.name;
+    var name=req.query.name;
     names.push(name);
     //JSON Javascript Object Notation
     res.send(JSON.stringify(names));
