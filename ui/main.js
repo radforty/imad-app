a6f5350665ebf1e0093afd247ab8b3eac2d3df3b
radforty <30650225+radforty@users.noreply.github.com>
2017-08-23 22:33:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ button.onclick = function(){
 //submit name
 
 var nameInput = document.getElementById('name');
-var name = nameInput.value;
+var name = nameInput.Value;
 var submit = document.getElementById('submit_btn');
 submit.onClick=function(){
     
