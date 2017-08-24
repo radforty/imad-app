35f1179696cca83bc499074c4ac4ff8bf65ca8d3
radforty <30650225+radforty@users.noreply.github.com>
2017-08-24 19:51:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,13 +100,13 @@ var pool=new Pool(config);
 app.get('/test-db', function (req, res) {
   //make a select request
   //return a response results
-  pool.query("SELECT * FROM test", function(err, result.rows){
+  pool.query("SELECT * FROM test", function(err, result){
              if(err){
                  res.status(500).send(err.toString());
              }
       else
       {
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
       }
 });
 });
