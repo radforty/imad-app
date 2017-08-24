0af6b26e9b75472cf3da90295196a10fcda02082
radforty <30650225+radforty@users.noreply.github.com>
2017-08-24 21:50:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,8 +127,8 @@ app.get('/submit-name', function(req,res){ // URL Submit-name/name:xxxx
 
 app.get('articles/:articleName', function (req, res) {
     //articleName=article-one
-    
-    pool.query("SELECT * FROM article where title =" + req.params.articleName, function(err, result){
+    //select * from article where title='article-one'
+    pool.query("SELECT * FROM article where title ='"+ req.params.articleName+"'", function(err, result){
              if(err){
                  res.status(500).send(err.toString());
              }
