be4ad16f9952e0ace319be65579d538589bbf27e
radforty <30650225+radforty@users.noreply.github.com>
2017-08-25 22:45:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ var username = req.body.username;
     var password = req.body.password;
     //var salt = crypto.randomBytes(128).toString('hex');
    // var dbString = hash(password, salt);
-    pool.query('SELECT * FROM "userChecking" username = $1', [username], function(err,result) {
+    pool.query('SELECT * FROM "userChecking" where username = $1', [username], function(err,result) {
                                  if(err){
                                         res.status(500).send(err.toString());
                                         }
