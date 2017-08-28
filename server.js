e78e6af75bbde10b01e598ec9410692925b3ada0
radforty <30650225+radforty@users.noreply.github.com>
2017-08-28 18:39:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user:'helloradhika',
@@ -16,6 +17,12 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session(
+    {
+    secret: 'SomeRandomSecretValue',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 }
+     }
+     ));
 var articles = {
 'article-one' : { title : 'Article by Radhika',
     heading : 'Article One',
