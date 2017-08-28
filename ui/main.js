5b2d1886b110fd59f8dd536dd5a1ae054d9047be
radforty <30650225+radforty@users.noreply.github.com>
2017-08-28 18:28:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,5 +34,6 @@ submit.onclick=function(){
          console.log(username);
           console.log(password);
           request.open('POST', 'http://helloradhika.imad.hasura-app.io/login', true);
+          request.setRequestHeader('Content-Type','application/json');
          request.send(JSON.stringify({username: username, password: password}));
         };
