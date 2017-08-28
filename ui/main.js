188c2149b2d34947b4e61b794ef6c1ffa5642ba6
radforty <30650225+radforty@users.noreply.github.com>
2017-08-28 18:19:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,22 +13,26 @@ submit.onclick=function(){
         //capture the response and store it in a variable
          request.onreadystatechange=function(){
                  if (request.readyState === XMLHttpRequest.DONE){
-                   //take some action
-                      if(request.status === 200){
-                      var names = request.responseText;
-                      names=JSON.parse(names);
-                       var list='';
-                                   for(var i=0; i<names.length; i++) {
-                                   list += '<li>' + names[i] + '</li>';
-                                   }
-                      var ul = document.getElementById('namelist');
-                       ul.innerHTML=list;
-                      }
-           }
+                             //take some action
+                         if(request.status === 200){
+                           alert('Logged in Successfully');
+                         }
+                         else
+                           if (request.status === 403)
+                            {
+                              alert('username/password is invalid');
+                            }
+                              else if (request.status === 500)
+                             {
+                               alert('username/password is invalid');
+                             }
+                   }
          };
          var nameInput = document.getElementById('name');
          var username = getElementById('username').value;
          var password = getElementById('password').value;
-          request.open('POST', 'http://helloradhika.imad.hasura-app.io/submit-name?name='+name, true);
+         console.log(username);
+          console.log(password);
+          request.open('POST', 'http://helloradhika.imad.hasura-app.io/login', true);
          request.send(JSON.stringify({username: username, password: password}));
         };
